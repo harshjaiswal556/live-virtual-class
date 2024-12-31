c6fed4e87880d66bc6dfcf3de7ea42d620a54a85
Harsh Shah Jaiswal <tech.harsh2003@gmail.com>
2024-12-31 12:53:00 +0530

    name set

---
+++ js/index-js.js
@@ -0,0 +1,15 @@
+var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {})
+
+if(localStorage.getItem("name")===null){
+
+    myModal.toggle()
+}else{
+    document.getElementById("user-name").innerText = "Hi "+localStorage.getItem("name")
+}
+
+
+
+function save(){
+    var name = document.getElementById("name").value;
+    localStorage.setItem("name", name);
+}
